2de4f482f8490278af54dff98bf0c253a46e6f77
Will Carle <wcarle88@gmail.com>
2015-01-10 18:04:38 -0500

    Basic chat features & user tracking
    Added user/message tracking

--- index.js
+++ index.js
@@ -6,18 +6,53 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = [];
+var messages = [];
+
 io.on('connection', function(socket){
 	console.log('User Connected');
 	socket.on('disconnect', function(){
 		console.log('User Disconnected');
+		var user = getUser(socket.id);
+		if(user){
+			users.splice(users.indexOf(user), 1);
+			io.emit('user_left', user.username);
+		}
+		else{
+			console.log("user not found");
+		}
 	})
 
 	socket.on('send_message', function (msg) {
-		console.log('message: ' + msg)
-		socket.emit('recieve_message', msg)
+		console.log('message: ' + msg.username + ': ' + msg.message)
+		addMessage(msg);
+		io.emit('recieve_message', msg)
+	})
+	socket.on('user_join', function (username) {
+		console.log('user joined: ' + username);
+		var user = {id: socket.id, username: username};
+		users.push(user);
+		socket.broadcast.emit('user_joined', user);
+		socket.emit('init', {users: users, messages: messages});
 	})
 })
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+function addMessage(message){
+	if(messages.length == 20){
+		messages.splice(0, 1);
+	}
+	messages.push(message);
+}
+function getUser(socket){
+	var found = null;
+	users.forEach(function (user, i) {
+		if(user.id == socket){
+			found = user;
+			return;
+		}
+	})
+	return found;
+}
